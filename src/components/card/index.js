ce2df871eb20577093bf9d371a3534c2ac4326ac
duckets615 <33557104+duckets615@users.noreply.github.com>
2019-03-13 10:10:52 -0400

    player card struct

---
+++ src/components/card/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+
+const { name, age, prefFoot, value, rating, games, goals, postion } = this.props;
+
+const PlayerCard = props => {
+  return (
+    <div className="playerCard">
+
+      <img src="./20801.png" alt=""/>
+      <h2 className='cardName'><strong>{name}</strong></h2>
+
+      <h3 className="cardAge">Age</h3>
+      <h4 className="cardAgeProp">{age}</h4>
+      <h3 className="prefFoot">Preferred Foot</h3>
+      <h4 className="prefFootProp">{prefFoot}</h4>
+
+      <h3 className="cardValue">Value</h3>
+      <h4 className="valueProp">{value}</h4>
+      <h3 className="cardRating">Rating</h3>
+      <h4 className="cardRatingProp">{rating}</h4>
+
+      <h3 className="cardGames">Games</h3>
+      <h4 className="cardGamesProp">{games}</h4>
+      <h3 className="cardGoals">Goals</h3>
+      <h4 className="cardGoalsProp">{goals}</h4>
+
+      <h3 className="cardPosition">Position</h3>
+      <h4 className="cardPositionProp">{position}</h4>
+
+      <a className='statLink' href="link to full stat page"></a>
+
+    </div>
+  );
+};
+
+
+
+//
+export default PlayerCard;
